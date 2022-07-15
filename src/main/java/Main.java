504fc484eb47950f460889f7483cf13bd97a873e
Daniil Shilov <shilov.da2001@mail.ru>
2022-07-16 00:13:25 +0300

    Small code refactoring

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,58 @@ import java.util.regex.Pattern;
 
 public class Main {
 
+  private static class Group {
+    private final List<List<String>> group;
+
+    private void addUniqueInGroup(List<List<String>> uncheckedGroup) {
+      for (List<String> uncheckedGroupRow : uncheckedGroup) {
+        boolean equal = false;
+        for (List<String> checkedGroupRow : group) {
+          if (uncheckedGroupRow.equals(checkedGroupRow)) {
+            equal = true;
+            break;
+          }
+        }
+        if (!equal) {
+          group.add(uncheckedGroupRow);
+        }
+      }
+    }
+
+    public Group(List<List<String>> rowsList, int startIndex, int endIndex) {
+      this.group = new ArrayList<>(endIndex - startIndex);
+      List<List<String>> uncheckedGroup = rowsList.subList(startIndex, endIndex + 1);
+      addUniqueInGroup(uncheckedGroup);
+    }
+
+    public int size() {
+      return this.group.size();
+    }
+
+    public boolean merge(Group b, int column) {
+      for (int i = 0; i < this.size(); i++) {
+        for (int j = 0; j < b.size(); j++) {
+          if (this.group.get(i).get(column).equals(b.group.get(j).get(column))) {
+            addUniqueInGroup(b.group);
+            return true;
+          }
+        }
+      }
+      return false;
+    }
+
+    @Override
+    public String toString() {
+      StringBuilder stringBuilder = new StringBuilder();
+      group.forEach(row -> {
+        row.forEach(string -> stringBuilder.append("\"").append(string).append("\";"));
+        stringBuilder.delete(stringBuilder.length() - 1, stringBuilder.length());
+        stringBuilder.append("\n");
+      });
+      return stringBuilder.toString();
+    }
+  }
+
   public static void main(String[] args) {
     final int ELEMENTS_COUNT_IN_ROW = 3;
     final String SEPARATOR = File.separator;
@@ -26,15 +78,18 @@ public class Main {
           continue;
         }
 
-        List<String> strings = Arrays.asList(line.split("\";\""));
-        if (strings.size() != ELEMENTS_COUNT_IN_ROW) {
+        List<String> row = Arrays.asList(line.split("\";\""));
+        int rowSize = row.size();
+        if (rowSize != ELEMENTS_COUNT_IN_ROW) {
           line = reader.readLine();
           continue;
         }
-        strings.set(0, strings.get(0).substring(1));
-        strings.set(strings.size() - 1, strings.get(strings.size() - 1).substring(0, strings.get(strings.size() - 1).length() - 1));
+        --rowSize;
+        row.set(0, row.get(0).substring(1));
+        row.set(rowSize, row.get(rowSize).substring(0, row.get(rowSize).length() - 1));
 
-        rowsList.add(strings);
+        rowsList.add(row);
+
         line = reader.readLine();
       }
     } catch (FileNotFoundException e) {
@@ -48,58 +103,6 @@ public class Main {
       return;
     }
 
-    class Group {
-      private final List<List<String>> group;
-
-      private void addUniqueInGroup(List<List<String>> uncheckedGroup) {
-        for (List<String> uncheckedGroupRow : uncheckedGroup) {
-          boolean equal = false;
-          for (List<String> checkedGroupRow : group) {
-            if (uncheckedGroupRow.equals(checkedGroupRow)) {
-              equal = true;
-              break;
-            }
-          }
-          if (!equal) {
-            group.add(uncheckedGroupRow);
-          }
-        }
-      }
-
-      public Group(List<List<String>> rowsList, int startIndex, int endIndex) {
-        group = new ArrayList<>(endIndex - startIndex);
-        List<List<String>> uncheckedGroup = rowsList.subList(startIndex, endIndex + 1);
-        addUniqueInGroup(uncheckedGroup);
-      }
-
-      public int size() {
-        return group.size();
-      }
-
-      public boolean merge(Group b, int column) {
-        for (int i = 0; i < this.size(); i++) {
-          for (int j = 0; j < b.size(); j++) {
-            if (this.group.get(i).get(column).equals(b.group.get(j).get(column))) {
-              addUniqueInGroup(b.group);
-              return true;
-            }
-          }
-        }
-        return false;
-      }
-
-      @Override
-      public String toString() {
-        StringBuilder stringBuilder = new StringBuilder();
-        group.forEach(row -> {
-          row.forEach(string -> stringBuilder.append("\"").append(string).append("\";"));
-          stringBuilder.delete(stringBuilder.length() - 1, stringBuilder.length());
-          stringBuilder.append("\n");
-        });
-        return stringBuilder.toString();
-      }
-    }
-
     List<List<Group>> groups = new ArrayList<>(ELEMENTS_COUNT_IN_ROW);
     for (int column = 0; column < ELEMENTS_COUNT_IN_ROW; column++) {
       int finalColumn = column;
@@ -111,7 +114,9 @@ public class Main {
       int startIndex = 0;
       int endIndex = 0;
       for (int i = 1; i < rowsList.size(); i++) {
-        if (!startGroupString.isEmpty() && !rowsList.get(i).get(column).isEmpty() && startGroupString.equals(rowsList.get(i).get(column))) {
+        String otherString = rowsList.get(i).get(column);
+        if (!startGroupString.isEmpty() && !otherString.isEmpty()
+            && startGroupString.equals(otherString)) {
           endIndex = i;
         } else {
           if (endIndex - startIndex != 0) {
@@ -120,7 +125,7 @@ public class Main {
               groups.get(column).add(group);
             }
           }
-          startGroupString = rowsList.get(i).get(column);
+          startGroupString = otherString;
           startIndex = i;
           endIndex = startIndex;
         }
@@ -145,8 +150,7 @@ public class Main {
       for (int i = 0; i < groups.get(mergeColumn).size(); i++) {
         writer.write("Group " + (i + 1) + "\n" + groups.get(mergeColumn).get(i) + "\n");
       }
-    }
-    catch (IOException e) {
+    } catch (IOException e) {
       System.out.println(e.getMessage());
     }
 
